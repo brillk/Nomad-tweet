7cc05bc05cfadc6149b952e077fd10433e5728b7
KJH <stonehead334@gmail.com>
2022-05-11 10:18:34 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
+
+//로그인 되었을때 의 home 화면 , 그리고
+//로그아웃일때 보여지는 로그인화면 주소를 구현
