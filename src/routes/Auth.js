7d7a62f7f5efb973a61f5d86fbdc6f518e830fbf
KJH <stonehead334@gmail.com>
2022-05-11 13:39:28 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,20 +1,42 @@
 import React, { useState } from "react";
+import { authService } from "fbase";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
+const auth = getAuth();
+//persistance 사용자를 어떻게 기억할지 선택하게 해준다?
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
   const onChange = event => {
     const {
       target: { name, value },
     } = event;
     if (name === "email") {
+      //이름이 email이라면 혹은 비밀번호라면
       setEmail(value);
     } else if (name === "password") {
       setPassword(value);
-    }
+    } //input에 입력하면 계속 업뎃을 하면서 글자가 써진다
   };
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(authService, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
@@ -35,7 +57,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log in" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
